c8b24bb045fc190d28f787f136529418b22f4da4
UtkarshReddyNathala <utkarshnathala@gmail.com>
2026-03-15 16:07:31 +0530

    Update pair_selection.py

--- pair_selection.py
+++ pair_selection.py
@@ -1,1 +1,24 @@
+from statsmodels.tsa.stattools import coint
 
+
+def find_best_pair(price_df):
+
+    instruments = price_df.columns
+
+    best_pair = None
+    best_pvalue = 1
+
+    for i in range(len(instruments)):
+        for j in range(i + 1, len(instruments)):
+
+            s1 = price_df[instruments[i]]
+            s2 = price_df[instruments[j]]
+
+            score, pvalue, _ = coint(s1, s2)
+
+            if pvalue < best_pvalue:
+
+                best_pvalue = pvalue
+                best_pair = (instruments[i], instruments[j])
+
+    return best_pair
